d0f83e0b3ece9e632231477b348d7caba8f1005f
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2007-02-21 00:35:36 +0000

    Fixed bug with init script.  Added funkload testing doco and scripts.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@12 7ee303d9-4526-0410-93f5-a5c2be67a987

---
+++ testing/test_whitelist.py
@@ -0,0 +1,94 @@
+# -*- coding: iso-8859-15 -*-
+"""Whitelist Tests
+
+"""
+import unittest
+import urllib
+from random import random
+from base64 import encodestring
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+from funkload.Lipsum import Lipsum
+
+class whitelist(FunkLoadTestCase):
+    """This test uses whitelist.conf."""
+
+    def setUp(self):
+        """Setting up whitelist test."""
+        self.title=self.conf_get('main', 'title')
+        self.logd("Setting up test: %s\n" % self.title)
+        self.user = self.conf_get('main', 'proxy_username')
+        self.passwd = self.conf_get('main', 'proxy_password')
+        self.basic_auth=encodestring(self.user+":"+self.passwd)
+        self.lipsum = Lipsum()
+
+    def test_viewwebpages(self):
+        # The description should be set in the configuration file
+        # begin of test ---------------------------------------------
+        nb_time = self.conf_getInt('test_viewwebpages', 'nb_time')
+        urls = self.conf_getList('test_viewwebpages', 'urls')
+
+        for i in range(nb_time):
+            self.logd('Try %i' % i)
+            for url in urls:
+                self.addHeader("Proxy-Authorization","Basic %s" % self.basic_auth)
+                response=self.get("http://"+url, description='Get %s' % url)
+                self.assert_(response.body.find("<img")>=0,"Page returned with no <img> tags.  Probably means the request failed.")
+
+
+        # end of test -----------------------------------------------
+
+    def test_viewwhitelist(self):
+        # The description should be set in the configuration file
+        # begin of test ---------------------------------------------
+        nb_time = self.conf_getInt('test_viewwhitelist', 'nb_time')
+        whitelist_url = self.conf_get('test_viewwhitelist', 'url')
+
+        for i in range(nb_time):
+            self.logd('Try %i' % i)
+            self.addHeader("Proxy-Authorization","Basic %s" % self.basic_auth)
+            response=self.get(whitelist_url, description='Get whitelist')
+            self.assertEquals(response.getDOM().getByName('title')[0][0],"HTTP Whitelist Report","Expected 'HTTP Whitelist Report' in HTML title'")
+
+        # end of test -----------------------------------------------
+
+    def test_addtowhitelist(self):
+        # The description should be set in the configuration file
+        # begin of test ---------------------------------------------
+        num_pages = self.conf_getInt('test_addtowhitelist', 'num_pages')
+        prefix = self.conf_get('test_addtowhitelist', 'prefix')
+        suffix = self.conf_get('test_addtowhitelist', 'suffix')
+        
+        for i in range(num_pages):
+            self.setHeader("Proxy-Authorization","Basic %s" % self.basic_auth)
+
+            page = self.lipsum.getUniqWord(length_min=5,length_max=40)
+            url="http://www."+page+".com"
+            self.logd('Requesting page %i: %s' % (i,url))
+            response=self.get(url, description='Get %s' % url)
+            self.assertEquals(response.getDOM().getByName('title')[0][0],"Internet Access Request Form","Expected 'Internet Access Request Form' in HTML title'")
+    
+            #Bizzare.  First param gets \r\n appended to the front.  To workaround I added a rubbish param "a".  This will be first as they get ordered alphabetically.
+            postparams=[['domain','www.'+page+'.com'],
+                        ['user',self.user],
+                        ['comment',urllib.quote_plus(self.lipsum.getSentence())],
+                        ['url',urllib.quote_plus(url)],
+                        ['consent','I+Agree']]
+            
+            self.logd('Adding page %i: %s' % (i,postparams))
+            response=self.post("http://whitelistproxy/",params=postparams,description='Post params:%s' % postparams,postformat='application/x-www-form-urlencoded')
+            self.assertEquals(response.getDOM().getByName('title')[0][0],"Access Granted","Expected 'Access Granted' in HTML title'")
+            self.assert_(response.body.find(url)>=0,"URL %s expected but not found in response" % url)
+            self.assert_(response.body.find(self.user)>=0,"Username %s expected but not found in response" % self.user)
+
+        # end of test -----------------------------------------------
+
+    def tearDown(self):
+        """Finishing test."""
+        self.logd("Teardown test: %s\n" % self.title)
+
+
+if __name__ in ('main', '__main__'):
+    unittest.main()
+
+
+
