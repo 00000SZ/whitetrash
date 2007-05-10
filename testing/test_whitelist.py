106e76648064c996cecb460e58ec347666d6357c
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2007-05-10 07:19:53 +0000

    Changed test suites to support SSL.  Unfortunately funkload changes are required before this will work.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@29 7ee303d9-4526-0410-93f5-a5c2be67a987

--- testing/test_whitelist.py
+++ testing/test_whitelist.py
@@ -22,8 +22,7 @@ class whitelist(FunkLoadTestCase):
         self.lipsum = Lipsum()
 
     def test_viewwebpages(self):
-        # The description should be set in the configuration file
-        # begin of test ---------------------------------------------
+        
         nb_time = self.conf_getInt('test_viewwebpages', 'nb_time')
         urls = self.conf_getList('test_viewwebpages', 'urls')
 
@@ -34,12 +33,10 @@ class whitelist(FunkLoadTestCase):
                 response=self.get("http://"+url, description='Get %s' % url)
                 self.assert_(response.body.find("<img")>=0,"Page returned with no <img> tags.  Probably means the request failed.")
 
-
-        # end of test -----------------------------------------------
-
     def test_viewwhitelist(self):
-        # The description should be set in the configuration file
-        # begin of test ---------------------------------------------
+        """This test is pretty CPU intensive as it parses the whole page each time looking for a /table tag (which is at the very end) to make sure we got a complete page.
+        Might be a better way to do this...."""
+        
         nb_time = self.conf_getInt('test_viewwhitelist', 'nb_time')
         whitelist_url = self.conf_get('test_viewwhitelist', 'url')
         self.addHeader("Proxy-Authorization","Basic %s" % self.basic_auth)
@@ -59,14 +56,11 @@ class whitelist(FunkLoadTestCase):
                 first_timestamp=timestamp
             self.assertEquals(timestamp,first_timestamp,"Caching check failed.  Page should not have regenerated.  Timestamp should be %s, but got %s" % (first_timestamp,timestamp))
 
-        # end of test -----------------------------------------------
+    def add_pages(self,protocol="HTTP"):
 
-    def test_addtowhitelist(self):
-        # The description should be set in the configuration file
-        # begin of test ---------------------------------------------
-        num_pages = self.conf_getInt('test_addtowhitelist', 'num_pages')
-        prefix = self.conf_get('test_addtowhitelist', 'prefix')
-        suffix = self.conf_get('test_addtowhitelist', 'suffix')
+        num_pages = self.conf_getInt('test_addto%swhitelist' % protocol, 'num_pages')
+        prefix = self.conf_get('test_addto%swhitelist' % protocol, 'prefix')
+        suffix = self.conf_get('test_addto%swhitelist' % protocol, 'suffix')
         
         for i in range(num_pages):
             self.addHeader("Accept","text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5")
@@ -78,25 +72,44 @@ class whitelist(FunkLoadTestCase):
             self.setHeader("Proxy-Authorization","Basic %s" % self.basic_auth)
 
             page = self.lipsum.getUniqWord(length_min=5,length_max=40)
-            url="http://www."+page+".com"
-            self.logd('Requesting page %i: %s' % (i,url))
+            self.setHeader("Host","www.%s.com" % page)
+            
+            if protocol=="HTTP":
+                url="http://www."+page+".com"
+            elif protocol=="SSL":
+                #Funkload picks up that this is SSL by the https identifier, we don't need to tell it explicitly.
+                url="https://www."+page+".com"
+                
+            self.logd('Requesting %s page %i: %s' % (protocol,i,url))
             response=self.get(url, description='Get %s' % url)
             self.assertEquals(response.getDOM().getByName('title')[0][0],"Internet Access Request Form","Expected 'Internet Access Request Form' in HTML title'")
     
-            #Bizzare.  First param gets \r\n appended to the front.  To workaround I added a rubbish param "a".  This will be first as they get ordered alphabetically.
-            postparams=[['domain','www.'+page+'.com'],
+            if protocol=="HTTP":
+                postparams=[['domain','www.'+page+'.com'],
                         ['user',self.user],
                         ['comment',urllib.quote_plus(self.lipsum.getSentence())],
                         ['url',urllib.quote_plus(url)],
+                        ['protocol',protocol],
                         ['consent','I+Agree']]
-            
-            self.logd('Adding page %i: %s' % (i,postparams))
-            response=self.post("http://whitelistproxy/",params=postparams,description='Post params:%s' % postparams)
+
+                self.logd('Adding %s page %i: %s' % (protocol,i,postparams))
+                response=self.post("http://whitelistproxy/",params=postparams,description='Post params:%s' % postparams)
+
+            elif protocol=="SSL":
+                #Emulate the actual process here by submitting user=ssl.
+                self.logd('Adding %s page %i' % (protocol,i))
+                response=self.post("https://whitelistproxy/",params=None)
+
             self.assertEquals(response.getDOM().getByName('title')[0][0],"Access Granted","Expected 'Access Granted' in HTML title'")
             self.assert_(response.body.find(urllib.quote_plus(url))>=0,"URL %s expected but not found in response" % urllib.quote_plus(url))
             self.assert_(response.body.find(self.user)>=0,"Username %s expected but not found in response" % self.user)
 
-        # end of test -----------------------------------------------
+    def test_addtoHTTPwhitelist(self):
+        self.add_pages(protocol="HTTP")
+    
+    def test_addtoSSLwhitelist(self):
+        #self.add_pages(protocol="SSL")
+        pass
 
     def tearDown(self):
         """Finishing test."""
