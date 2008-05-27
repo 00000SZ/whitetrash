ba1dcc92b026716c70719847cc62428bb4604070
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2008-05-27 00:59:21 +0000

    Some progress on plugin.  Listing domains, but not tab-specific.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@63 7ee303d9-4526-0410-93f5-a5c2be67a987

--- firefox_plugin/chrome/content/browser_overlay.js
+++ firefox_plugin/chrome/content/browser_overlay.js
@@ -2,76 +2,116 @@
  * Protect the javascript global namespace!
  * http://blogger.ziesemer.com/2007/10/respecting-javascript-global-namespace.html
  * **/
+
 var whitetrashOverlay=null;
 
 whitetrashOverlay = {
 
-    createMenuItem: function(aPopup) {
-
-        var domainDupChecker = {
-            domains: {},
-            check: function(d) {
-                return this.domains[d] || !(this.domains[d] = true);
-            }
-        };
- 
-        var item = document.createElement("menuitem"); // create a new XUL menuitem
-        //whitetrashOverlay.getBlockedDomains();
-        var domain="temptest.com";
-        if (!domainDupChecker.check(domain)) {
-            item.setAttribute("label", domain);
-            item.setAttribute("class", "menuitem-iconic whitetrash-can");
-            item.setAttribute("oncommand", "whitetrashOverlay.addToWhitelist(\"blah.com\")");
-            aPopup.appendChild(item);
+    domainDupChecker: {
+        domains: {},
+        isDup: function(d) {
+            return this.domains[d] || !(this.domains[d] = true);
         }
+    }
+,
+    createMenuItem: function(aPopup,domain,uri,protocol) {
 
+        var item = document.createElement("menuitem"); // create a new XUL menuitem
+        item.setAttribute("label", domain);
+        item.setAttribute("class", "menuitem-iconic whitetrash-can");
+        item.setAttribute("oncommand", "whitetrashOverlay.addToWhitelist("+domain+protocol+uri+")");
+        aPopup.appendChild(item);
 
     }//createMenuItem
 ,
-    onContentLoad: function() {
+    deleteDynamicMenuItems: function(popup) {
 
         var logger = Components.classes["@mozilla.org/consoleservice;1"].
-        getService(Components.interfaces.nsIConsoleService)
-        try{
-            //var =document.getElementById("host");
-            var frames = window.frames; // or // var frames = window.parent.frames;
-            logger.logStringMessage("frames.length:"+frames.length);
-            for (var i = 0; i < frames.length; i++) { 
-            // do something with each subframe as frames[i]
-                var wtdomain={};
-                logger.logStringMessage("frame "+i+" content:"+frames[i].document.body.innerHTML); 
-                if (wtdomain = frames[i].document.getElementById("host")) {
-                    alert("ssdf"+wtdomain.innerHTML);
-                }
-            }
+        getService(Components.interfaces.nsIConsoleService);
 
-        
-        }catch(e){
-            try{
-                if(e.stack){
-                Components.utils.reportError(e.stack);
-                }
-                // Show the error console.
-                toJavaScriptConsole();
-            }finally{
-                throw e;
-            }
+        logger.logStringMessage("Removing domains");
+        whitetrashOverlay.domainDupChecker.domains={};
+        var dynamic_sep = document.getElementById("dynamic_content_sep");
+        while (popup.lastChild != dynamic_sep) {
+            logger.logStringMessage("Removing id: "+popup.lastChild.id);
+        	popup.removeChild(popup.lastChild);
+        }
+    }
+,
+    parseHTML: function(wt_sb_menu_popup,tag_name,attribute,this_doc) {
 
+        var logger = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService);
+
+
+        var tags = this_doc.getElementsByTagName(tag_name); 
+        domain_re = /^(https?):\/\/(([a-z0-9-]{1,50}\.){1,6}[a-z]{2,6})\//;
+        for (var i = 0; i < tags.length; i++) { 
+            uri = tags[i].getAttribute(attribute); 
+            var domain=null;
+            if (domain=domain_re.exec(uri)) {
+                if (!whitetrashOverlay.domainDupChecker.isDup(domain[2])) {
+                    logger.logStringMessage("Good domain: "+domain[2],uri,domain[0]);
+                    whitetrashOverlay.createMenuItem(wt_sb_menu_popup,domain[2],uri,domain[0])
+                }
+            } else {
+                logger.logStringMessage("Bad domain: "+uri);
+            }
         }
 
+    }
+,
+    onContentLoad: function(ev) {
 
-    }//getBlockedDomains
+        var doc = ev.originalTarget;
+        if (doc instanceof HTMLDocument) {
+
+            var logger = Components.classes["@mozilla.org/consoleservice;1"].
+            getService(Components.interfaces.nsIConsoleService)
+
+                var wt_sb_menu_popup = document.getElementById("wt_sb_menu");
+                whitetrashOverlay.parseHTML(wt_sb_menu_popup,"iframe","src",doc);
+                whitetrashOverlay.parseHTML(wt_sb_menu_popup,"img","src",doc);
+                whitetrashOverlay.parseHTML(wt_sb_menu_popup,"script","src",doc);
+                whitetrashOverlay.parseHTML(wt_sb_menu_popup,"link","href",doc);
+            
+        }
+
+    }
 ,
-    addToWhitelist: function(domain) {
+    tabChanged: function(ev) {
+        var logger = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService)
+        logger.logStringMessage("Tab change");
+        var wt_sb_menu_popup = document.getElementById("wt_sb_menu");
+        whitetrashOverlay.deleteDynamicMenuItems(wt_sb_menu_popup);
+    }
+,
+    addToWhitelist: function(domain,protocol,uri) {
         alert('added domain:'+domain);
-    }//addToWhitelist
+    }
 ,
-    install: function() {
-        // this.wt.dump("*** OVERLAY INSTALL ***\n");
+    onLoad: function(ev) {
+        var logger = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService)
+        logger.logStringMessage("Onload fired");
+        window.removeEventListener("load", arguments.callee, false);
         var prefs = this.prefService = Components.classes["@mozilla.org/preferences-service;1"]
             .getService(Components.interfaces.nsIPrefService).getBranch("whitetrash.");
-        
-        window.addEventListener("DOMFrameContentLoaded", this.onContentLoad, true);
+
+        gBrowser.mPanelContainer.addEventListener("TabSelect", this.tabChanged, false);
+    }   
+,
+    install: function() {
+        /** Contrary to http://developer.mozilla.org/en/docs/Gecko-Specific_DOM_Events 
+         * DOMFrameContentLoaded is not "the same as DOMContentLoaded, but also fired for enclosed frames."
+         * it was only giving me empty document objects. 
+         * **/
+        window.addEventListener("DOMContentLoaded", this.onContentLoad, false);
+
+    	//Don't want to execute too early.  Wait until window has loaded.
+        window.addEventListener("load", this.onLoad, false);
+
     }
 
 }//end namespace encapsulation
