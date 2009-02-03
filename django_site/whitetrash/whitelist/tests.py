ccdc43ace79f3805806a796a5296fec8e0e5be58
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2009-02-03 11:01:31 +0000

    Work on server unittesting.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@92 7ee303d9-4526-0410-93f5-a5c2be67a987

--- django_site/whitetrash/whitelist/tests.py
+++ django_site/whitetrash/whitelist/tests.py
@@ -7,7 +7,19 @@ from django.test import TestCase
 
 
 class WhitetrashTestCase(TestCase):
+    fixtures = ["testing.json"]
 
-    def 
+    def testIndexRedirect(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, "http://whitetrash/whitelist/view/list/",
+                status_code=301, target_status_code=200)
     
+    def testFormNoLogin(self):
+        response = self.client.get('/whitelist/getform')
+        self.assertRedirects(response, "whitelist/getform/",
+                status_code=301, target_status_code=302)
+
+    def testFormLogin(self):
+        #TODO fix this - fixtures working?
+        self.client.login(username='testuser', password='passwd')
 
