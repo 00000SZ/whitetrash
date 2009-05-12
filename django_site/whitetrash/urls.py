69c9dd7cb44b05903c688dcff2f8191495b6199e
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2009-05-12 06:16:00 +0000

    Added download ca and logout options to sidebar.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@152 7ee303d9-4526-0410-93f5-a5c2be67a987

--- django_site/whitetrash/urls.py
+++ django_site/whitetrash/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root,{'SSL':True}),
     (r'^accounts/login/$', 'django.contrib.auth.views.login',
                 {'template_name': 'whitelist/login_form.html','SSL':True}),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+                {'template_name': 'whitelist/logout.html','SSL':True}),
+
 )
 
 
