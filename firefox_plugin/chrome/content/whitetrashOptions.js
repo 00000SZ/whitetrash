5b72b02ed83fa5dfe8058684394343dbdcc75608
gregsfdev <gregsfdev@7ee303d9-4526-0410-93f5-a5c2be67a987>
2009-06-03 08:14:53 +0000

    Work on options menu.
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@189 7ee303d9-4526-0410-93f5-a5c2be67a987

---
+++ firefox_plugin/chrome/content/whitetrashOptions.js
@@ -0,0 +1,17 @@
+/**
+ * Protect the javascript global namespace!
+ * http://blogger.ziesemer.com/2007/10/respecting-javascript-global-namespace.html
+ * **/
+
+var whitetrashOpt=null;
+
+whitetrashOpt = {
+    
+  init: function() {
+  }
+  ,
+  save: function() {
+  }
+
+}//end namespace encapsulation
+
