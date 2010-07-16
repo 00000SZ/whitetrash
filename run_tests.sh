c038f24cbdc52de92ce0cce498a64606b4d11d85
cford <cford@7ee303d9-4526-0410-93f5-a5c2be67a987>
2010-07-16 06:05:22 +0000

    Testing now imports whitetrash test-env/ will be ignored by svn
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@234 7ee303d9-4526-0410-93f5-a5c2be67a987

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,8 @@ virtualenv --distribute --no-site-packages test-env
 pip install -E test-env -r testing/pip-testing-req.txt
 source test-env/bin/activate
 
-python -c "import django"
+export DJANGO_SETTINGS_MODULE=django_site.whitetrash.settings
+
+python -c "import redirector.whitetrash2"
 
 deactivate
