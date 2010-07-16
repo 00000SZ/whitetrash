5d971717938ae4d3b4645d8c18a826fb293c61cf
cford <cford@7ee303d9-4526-0410-93f5-a5c2be67a987>
2010-07-16 06:14:19 +0000

    return proper exit code and get hudson to fail properly
    git-svn-id: svn://svn.code.sf.net/p/whitetrash/code/trunk@235 7ee303d9-4526-0410-93f5-a5c2be67a987

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@ virtualenv --distribute --no-site-packages test-env
 pip install -E test-env -r testing/pip-testing-req.txt
 source test-env/bin/activate
 
+export PYTHONPATH=`pwd`
 export DJANGO_SETTINGS_MODULE=django_site.whitetrash.settings
 
 python -c "import redirector.whitetrash2"
-
-deactivate
+exit $?
